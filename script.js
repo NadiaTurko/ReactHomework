d1f684a590d58a28b84a379437095ae58f32b2e2
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-08-23 11:59:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,168 +1,158 @@
-var domContainer = document.querySelector("#root");
-var root = ReactDOM.createRoot(domContainer);
+const domContainer = document.querySelector("#root");
+const root = ReactDOM.createRoot(domContainer);
 
-var titleCars = "Car Specs";
-var CARS = [{
-  id: 1,
-  brand: "Audi",
-  models: [{
+const titleCars = "Car Specs";
+const CARS = [
+  {
     id: 1,
-    name: "A1",
-    collection: [{
-      id: 1,
-      version: "Sportback",
-      year: 2019,
-      horsepower: 95,
-      engine: 999
-    }, {
-      id: 2,
-      version: "Citycarver",
-      year: 2019,
-      horsepower: 95,
-      engine: 999
-    }]
-  }, {
+    brand: "Audi",
+    models: [
+      {
+        id: 1,
+        name: "A1",
+        collection: [
+          {
+            id: 1,
+            version: "Sportback",
+            year: 2019,
+            horsepower: 95,
+            engine: 999,
+          },
+          {
+            id: 2,
+            version: "Citycarver",
+            year: 2019,
+            horsepower: 95,
+            engine: 999,
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Q5",
+        collection: [
+          {
+            id: 1,
+            version: "FY 2021",
+            year: 2021,
+            horsepower: 299,
+            engine: 1984,
+          },
+          {
+            id: 2,
+            version: "Sportback",
+            year: 2021,
+            horsepower: 299,
+            engine: 1984,
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "TT",
+        collection: [
+          {
+            id: 1,
+            version: "Coupe",
+            year: 2021,
+            horsepower: 197,
+            engine: 1984,
+          },
+          {
+            id: 2,
+            version: "Roadster",
+            year: 2021,
+            horsepower: 197,
+            engine: 1984,
+          },
+        ],
+      },
+    ],
+  },
+  {
     id: 2,
-    name: "Q5",
-    collection: [{
-      id: 1,
-      version: "FY 2021",
-      year: 2021,
-      horsepower: 299,
-      engine: 1984
-    }, {
-      id: 2,
-      version: "Sportback",
-      year: 2021,
-      horsepower: 299,
-      engine: 1984
-    }]
-  }, {
-    id: 3,
-    name: "TT",
-    collection: [{
-      id: 1,
-      version: "Coupe",
-      year: 2021,
-      horsepower: 197,
-      engine: 1984
-    }, {
-      id: 2,
-      version: "Roadster",
-      year: 2021,
-      horsepower: 197,
-      engine: 1984
-    }]
-  }]
-}, {
-  id: 2,
-  brand: "BMW",
-  models: [{
-    id: 1,
-    name: "8 series",
-    collection: [{
-      id: 1,
-      version: "G1X LCI",
-      year: 2022,
-      horsepower: 333,
-      engine: 2998
-    }, {
-      id: 2,
-      version: "G1X",
-      year: 2019,
-      horsepower: 340,
-      engine: 2998
-    }]
-  }, {
-    id: 2,
-    name: "X6",
-    collection: [{
-      id: 1,
-      version: "G06 LCI",
-      year: 2023,
-      horsepower: 530,
-      engine: 4395
-    }, {
-      id: 2,
-      version: "G06",
-      year: 2020,
-      horsepower: 286,
-      engine: 2993
-    }]
-  }]
-}];
+    brand: "BMW",
+    models: [
+      {
+        id: 1,
+        name: "8 series",
+        collection: [
+          {
+            id: 1,
+            version: "G1X LCI",
+            year: 2022,
+            horsepower: 333,
+            engine: 2998,
+          },
+          {
+            id: 2,
+            version: "G1X",
+            year: 2019,
+            horsepower: 340,
+            engine: 2998,
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "X6",
+        collection: [
+          {
+            id: 1,
+            version: "G06 LCI",
+            year: 2023,
+            horsepower: 530,
+            engine: 4395,
+          },
+          {
+            id: 2,
+            version: "G06",
+            year: 2020,
+            horsepower: 286,
+            engine: 2993,
+          },
+        ],
+      },
+    ],
+  },
+];
 
-var CarsTable = React.createElement(
-  React.Fragment,
-  null,
-  React.createElement(
-    "h1",
-    null,
-    titleCars
-  ),
-  React.createElement(
-    "table",
-    null,
-    React.createElement(
-      "tbody",
-      null,
-      CARS.map(function (mark, index) {
-        return React.createElement(
-          "tr",
-          { key: index },
-          React.createElement(
-            "td",
-            null,
-            mark.brand
-          ),
-          mark.models.map(function (carModel, i) {
-            return React.createElement(
-              "tr",
-              { key: i },
-              React.createElement(
-                "td",
-                null,
-                carModel.name
-              ),
-              React.createElement(
-                "td",
-                null,
-                carModel.collection.map(function (collectionItem, collectionIndex) {
-                  return React.createElement(
-                    "ul",
-                    { key: collectionIndex },
-                    React.createElement(
-                      "li",
-                      null,
-                      "Version: ",
-                      collectionItem.version
-                    ),
-                    React.createElement(
-                      "li",
-                      null,
-                      "Year: ",
-                      collectionItem.year
-                    ),
-                    React.createElement(
-                      "li",
-                      null,
-                      "Horsepower: ",
-                      collectionItem.horsepower
-                    ),
-                    React.createElement(
-                      "li",
-                      null,
-                      "Engine: ",
-                      collectionItem.engine
-                    )
-                  );
-                })
-              )
-            );
-          })
-        );
-      })
-    )
-  )
+const CarsTable = (
+  <React.Fragment>
+    <h1>{titleCars}</h1>
+    <table>
+      <tbody>
+        {CARS.map((mark, index) => (
+          <React.Fragment key={index}>
+            <tr className="row__mark" key={mark.id}>
+              <td colSpan={2}>{mark.brand}</td>
+            </tr>
+            {mark.models.map((carModel, i) =>
+              carModel.collection.map((collectionItem, collectionIndex) => (
+                <tr key={collectionIndex}>
+                  {!collectionIndex ? (
+                    <td rowSpan={carModel.collection.length}>
+                      {carModel.name}
+                    </td>
+                  ) : null}
+                  <td>
+                    <ul>
+                      {Object.keys(collectionItem)
+                        .filter((k) => k != `id`)
+                        .map((k, i) => (
+                          <li key={i}>
+                            {k}:{collectionItem[k]}
+                          </li>
+                        ))}
+                    </ul>
+                  </td>
+                </tr>
+              ))
+            )}
+          </React.Fragment>
+        ))}
+      </tbody>
+    </table>
+  </React.Fragment>
 );
 root.render(CarsTable);
